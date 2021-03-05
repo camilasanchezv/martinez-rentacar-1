0cfd47081799f7c3d50b75835dc311facb6c3f69
Matias <matintosh@Contrasenas-MacBook-Pro.local>
2021-03-05 16:14:14 -0300

    Adding structure, and node-sass.

---
+++ src/screens/Welcome/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Title } from '../../components'
+
+export default function Welcome() {
+    return (
+        <div>
+           <Title text="Martinez rent a car" />
+        </div>
+    )
+}
