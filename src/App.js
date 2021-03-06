95b91847a0e76b32e32a32897355194f64659a35
matintosh <matiasmartineeez@gmail.com>
2021-03-05 21:18:42 -0300

    Adding grommet and icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import {Welcome} from './screens';
+import { Welcome } from './screens';
+import { Grommet } from 'grommet';
+import 'typeface-roboto'
+
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <Welcome />
-    </div>
-  );
+    <Grommet theme={theme}>
+      <div className="App">
+        <Welcome />
+      </div>
+    </Grommet>
+  )
 }
 
 export default App;
--- src/screens/Welcome/Welcome.js
+++ src/screens/Welcome/Welcome.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { Title } from '../../components'
-
+import { FormField, TextInput, Form, Button, Box } from 'grommet'
+import { Icon } from 'react-icons-kit'
+import { home } from 'react-icons-kit/icomoon/home'
+import './styles.scss';
 export default function Welcome() {
+    const [value, setValue] = React.useState({});
     return (
-        <div>
-           <Title text="Martinez rent a car" />
+        <div className="customer-form">
+            <Form
+                value={value}
+                onChange={nextValue => setValue(nextValue)}
+                onReset={() => setValue({})}
+                onSubmit={({ value }) => { }}
+            >
+                <FormField name="name" htmlFor="text-input-id" label="Customer">
+                    <TextInput id="text-input-id" name="name" />
+                </FormField>
+                <Box direction="row" gap="medium">
+                    <Button type="submit" primary label="Submit" />
+                    <Button type="reset" label="Reset" />
+                </Box>
+            </Form>
         </div>
-    )
+    );
+
 }
