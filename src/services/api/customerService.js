a41f9b0ed16f83d5a2b46a825d1b35411e7d699b
Camila <camilasanchezvinik@hotmail.com>
2021-03-08 17:25:36 -0300

    Linked customer form with backend and made minor changes in button display code.

--- src/services/api/customerService.js
+++ src/services/api/customerService.js
@@ -1,16 +1,16 @@
 import { unauthAxiosCall } from '../axiosCall';
 
 export const listCustomers = async () => {
-    return unauthAxiosCall( 'customer/list', {
+    return unauthAxiosCall('customer/list', {
         method: "GET"
-    } )
+    })
 }
 
-export const createCustomer = async (firstName, lastName, phone, email) => {
-    return unauthAxiosCall( 'customer/create', {
+export const createCustomer = async (first_name, last_name, ci, phone, email, birthDate) => {
+    return unauthAxiosCall('customer/create', {
         method: "POST",
         body: {
-            firstName, lastName, phone, email
+            first_name, last_name, ci, phone, email, birthDate
         }
-    } )
+    })
 }
