61e40dcc452f396ddb571a3919185fa63198792e
matintosh <matiasmartineeez@gmail.com>
2021-03-12 23:32:58 -0300

    Applying functional programming to a side project xd

--- src/components/CustomersList/CustomersList.js
+++ src/components/CustomersList/CustomersList.js
@@ -26,54 +26,29 @@ export default function CustomersList() {
 
     // this could be better done? yes. Do i have not idea at all of how to do it? also yes.
 
-    function sortByName() {
-        if (context.customers) {
-            const aux = context.customers.map((a) => a)
-            setData(aux.sort((a, b) => (a.firstName.toLowerCase() > b.firstName.toLowerCase()) ? 1 : -1));
-        }
-    }
-    function sortByLastName() {
-        if (context.customers) {
-            const aux = context.customers.map((a) => a)
-            setData(aux.sort((a, b) => (a.lastName.toLowerCase() > b.lastName.toLowerCase()) ? 1 : -1));
-        }
-    }
-    function sortByEmail() {
-        if (context.customers) {
-            const aux = context.customers.map((a) => a)
-            setData(aux.sort((a, b) => (a.email.toLowerCase() > b.email.toLowerCase()) ? 1 : -1));
-        }
-    }
-    function sortByCI() {
-        if (context.customers) {
-            const aux = context.customers.map((a) => a)
-            setData(aux.sort((a, b) => (a.ci > b.ci) ? 1 : -1));
-        }
-    }
+
+
+    const sortTexts = (a, b, prop) => a[prop].toLowerCase() > b[prop].toLowerCase() ? 1 : -1;
+    const sortCustomers = sorting => context.customers.map(a => a).sort(sorting);
+    const sortBy = prop => setData(sortCustomers((a, b) => sortTexts(a, b, prop)));
+    
     function sortByBirthDate() { //this should be sorted by date
-        if (context.customers) {
-            const aux = context.customers.map((a) => a)
-            setData(aux.sort((a, b) => (a.birthDate > b.birthDate) ? 1 : -1));
+            if (context.customers) {
+                const aux = context.customers.map((a) => a)
+                setData(aux.sort((a, b) => (a.birthDate > b.birthDate) ? 1 : -1));
+            }
         }
-    }
-    function sortByPhone() {
-        if (context.customers) {
-            const aux = context.customers.map((a) => a)
-            setData(aux.sort((a, b) => (a.phone > b.phone) ? 1 : -1));
-        }
-    }
-
-
+        
     return (
         <div className="container" >
             <div className="customers-list">
                 <Header className="header" background={{ color: 'brand' }}>
-                    {width > 664 && <Box className="box"><Text className="header-item first-item">NOMBRE<Down className="arrow" color='accent' onClick={() => sortByName()} /></Text></Box>}
-                    {width > 493 && <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item" >APELLIDO<Down className="arrow" color='accent' onClick={() => sortByLastName()} /></Text></Box>}
-                    {width > 968 && <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item">EMAIL<Down className="arrow" color='accent' onClick={() => sortByEmail()} /></Text></Box>}
-                    <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item">DOCUMENTO<Down className="arrow" color='accent' onClick={() => sortByCI()} /></Text></Box>
+                    {width > 664 && <Box className="box"><Text className="header-item first-item">NOMBRE<Down className="arrow" color='accent' onClick={() => sortBy('firstName')} /></Text></Box>}
+                    {width > 493 && <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item" >APELLIDO<Down className="arrow" color='accent' onClick={() => sortBy('lastName')} /></Text></Box>}
+                    {width > 968 && <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item">EMAIL<Down className="arrow" color='accent' onClick={() => sortBy('email')} /></Text></Box>}
+                    <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item">DOCUMENTO<Down className="arrow" color='accent' onClick={() => sortBy('ci')} /></Text></Box>
                     {width > 1138 && <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item">NACIMIENTO<Down className="arrow" color='accent' onClick={() => sortByBirthDate()} /></Text></Box>}
-                    {width > 1308 && <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item">TELÉFONO<Down className="arrow" color='accent' onClick={() => sortByPhone()} /></Text></Box>}
+                    {width > 1308 && <Box className="box" border={{ side: 'left', color: 'accent', size: 'small' }}><Text className="header-item">TELÉFONO<Down className="arrow" color='accent' onClick={() => sortBy('phone')} /></Text></Box>}
                 </Header>
                 <Box className="scroll-container" height="medium" overflow="auto">
                     <InfiniteScroll state={data} items={data}>
