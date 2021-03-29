9f9324967d75cb33945ffe760604d88256816030
Camila <camilasanchezvinik@hotmail.com>
2021-03-29 02:31:29 -0300

    Some media fixes also applied in car-form.

--- src/components/UserSignUp/UserSignUp.js
+++ src/components/UserSignUp/UserSignUp.js
@@ -35,23 +35,23 @@ export default function UserForm() {
             >
                 <div className="user-form">
                     <div className="form-column">
-                        <FormField className="input" label={<Text className="label" >Nombre</Text>}>
-                            <TextInput className="placeholder" placeholder="Agustín" name="firstName" required />
+                        <FormField className="input" label="Nombre">
+                            <TextInput placeholder="Agustín" name="firstName" required />
                         </FormField>
-                        <FormField className="input" label={<Text className="label" >Apellido</Text>}>
-                            <TextInput className="placeholder" placeholder="Fernández" name="lastName" required />
+                        <FormField className="input" label="Apellido">
+                            <TextInput placeholder="Fernández" name="lastName" required />
                         </FormField>
-                        <FormField className="input" label={<Text className="label" >Teléfono</Text>}>
-                            <TextInput className="placeholder" type="number" name="phone" placeholder="26045555" required />
+                        <FormField className="input" label="Teléfono">
+                            <TextInput type="number" name="phone" placeholder="26045555" required />
                         </FormField>
                     </div>
 
                     <div className="form-column">
-                        <FormField className="input" type="email" label={<Text className="label" >Email</Text>}>
-                            <TextInput className="placeholder" name="email" placeholder="agustinfernandez@ejemplo.com" required />
+                        <FormField className="input" type="email" label="Email">
+                            <TextInput name="email" placeholder="agustinfernandez@ejemplo.com" required />
                         </FormField>
-                        <FormField className="input" label={<Text className="label" >Contraseña</Text>}>
-                            <div className="password-box focus" direction="row">
+                        <FormField className="input" label="Contraseña">
+                            < div className="password-box focus" direction="row" >
                                 <TextInput
                                     className="focus"
                                     plain
@@ -64,15 +64,15 @@ export default function UserForm() {
                                     icon={reveal ? <View size="medium" /> : <Hide size="medium" />}
                                     onClick={() => setReveal(!reveal)}
                                 />
-                            </div>
-                        </FormField>
-                    </div>
-                </div>
+                            </div >
+                        </FormField >
+                    </div >
+                </div >
 
                 <Box className="button" direction="row" gap="medium">
                     <Button type="submit" label="Registrar" primary />
                 </Box>
-            </Form>
+            </Form >
         </div >
     )
 }
